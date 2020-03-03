b4fb778760f75592f9d4f21b590e12b92bb535e2
Jason-Kyle De Lara <delara@unlv.nevada.edu>
2020-03-02 17:50:10 -0800

    fix deployment bugs

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 const path = require('path');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //Initialize Logger Middleware
 app.use(logger);
