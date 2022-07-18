aa297ca1f65a2f7b77887e79019a519a7bfce009
Jason-Kyle De Lara <jason8oh8@gmail.com>
2022-07-18 13:09:10 -0700

    Clean

--- middleware/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-    const targetUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-
-    console.log(`Loading: ${targetUrl}`);
-    next();
-};
-
-module.exports = logger;
