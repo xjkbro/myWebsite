cbee50f5a9101f173bf8cde74e42088c93e3c345
Jason-Kyle De Lara <jason8oh8@gmail.com>
2022-07-19 13:46:19 -0700

    funny

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 module.exports = {
-    prefix: "tw-",
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-    variants: {
-        extend: {},
+    colors: {
+      'primary': '#fb8500',
+      'secondary': '#ffb703',
+      'dark': '#212529',
+      'dark-lite': '#343a40'
     },
-    plugins: [],
-};
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
